ba7656b33b8c76955201c894ffeccaa1de27db1a
bryantbello <bryant.bello@outlook.com>
2016-05-05 13:18:36 -0400

    survey done

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friendArray = [{
     "name": "Bryant",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAebAAAAJDRiOWE0OTljLWQzZjUtNGZkZC1iZmY1LTk2Y2VmYzY3NWFjOA.jpg",
+    "photo": "https://pbs.twimg.com/profile_images/490504874996822016/u2byiHGx.jpeg",
     "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
 }];
 
